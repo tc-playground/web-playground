40db82ad6ef1cf297339214e3fd73981622379f3
Tim Langford <tim.langford@gmail.com>
2019-01-25 09:08:05 +0000

    Updating example.

--- react/javascript/example-02/counter-app/src/components/counter.jsx
+++ react/javascript/example-02/counter-app/src/components/counter.jsx
@@ -1,9 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World!</h1>;
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <button>Increment</button>
+      </div>
+    );
+  }
+
+// NB: ctrl-d for VSCode multi cursor.
+
+// React.Fragment: Use a child element of the target instead of a div.
+// 
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <h1>Hello World!</h1>
+//         <button>Increment</button>
+//       </React.Fragment>
+//     );
+//   }
+
 }
- 
+
 export default Counter;
