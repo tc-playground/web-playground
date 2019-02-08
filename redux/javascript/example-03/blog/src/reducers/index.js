aef98f6a5d4d01ebeffbcf1fcd5cfda200be487d
Tim Langford <tim.langford@gmail.com>
2019-02-08 07:08:41 +0000

    Updating.

--- redux/javascript/example-03/blog/src/reducers/index.js
+++ redux/javascript/example-03/blog/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { postReducer } from "./postReducer";
 
 export default combineReducers({
     // Dummy reducer to remove initial errors.
-    dummy: () => { return false; }
+    // dummy: () => { return false; },
+    posts: postReducer
 });
