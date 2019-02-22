89132c62f7a47dabada62e6cbd5cebf01f6868a2
Tim Langford <tim.langford@gmail.com>
2019-02-22 08:26:49 +0000

    Adding basic REduxForm inputs.

--- react-streams-app/client/src/components/streams/StreamCreate.js
+++ react-streams-app/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+  // formProps is wired in on ReduxForm.Field.
+  // renderInput(formProps) {
+  //   console.log("StreamCreate.renderInput(formProps) formProps:", formProps);
+  //   return (
+  //     <input onChange={formProps.input.onChange} value={formProps.value}/>
+  //   );
+  // }
+  // Short syntax - De-structure input, and, splat all it's properties.
+  renderInput({ input }) {
+    return (
+      <input {...input} />
+    );
+  }
+
+  render() {
+    console.log("StreamCreate this.props: ", this.props);
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
 }
 
-export default StreamCreate;
+// NB: Causes prop is Object not Function warning.
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
