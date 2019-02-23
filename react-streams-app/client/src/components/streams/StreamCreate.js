1039d65eea9ce31f6a0ac54fa35a269010f96a80
Tim Langford <tim.langford@gmail.com>
2019-02-23 12:22:40 +0000

    Added validation to streams creation.

--- react-streams-app/client/src/components/streams/StreamCreate.js
+++ react-streams-app/client/src/components/streams/StreamCreate.js
@@ -10,31 +10,55 @@ class StreamCreate extends React.Component {
   //   );
   // }
   // Short syntax - De-structure input, and, splat all it's properties.
-  renderInput({ input, label }) {
-
+  renderInput = ({ input, label, meta }) => {
+    console.log("StreamCreate.renderInput(formProps) input:", input);
+    console.log("StreamCreate.renderInput(formProps) label:", label);
+    console.log("StreamCreate.renderInput(formProps) meta:", meta);
+    const className = `field ${(meta.error && meta.touched) ? 'error' : ''}`
     return (
-      <div className="ui form">
+      <div className={className}>
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
+        { this.renderError(meta) }
       </div>
     );
   }
 
+  renderError({error, touched}) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div> 
+        </div>
+      )
+    }
+  }
+
   // Non ReduxForm version.
+  //
   // onSubmit(event) {
   //   event.preventDefault();
   // }
 
   // ReduxForm version. Handles some common tasks.
   onSubmit(formValues) {
-    console.log("StreamCreate.onSubmit(formValues) ", formValues)
+    console.log("StreamCreate.onSubmit(formValues) ", formValues);
   }
 
   render() {
     console.log("StreamCreate this.props: ", this.props);
     return (
-      <form clasName= "ui from" onsubmit={this.props.handleSubmit(this.onSubmit)}>
-        <Field name="title" component={this.renderInput} label="Enter title" />
+      <form
+        // NB: semantic-ui need `error` in-order to show errors. 
+        className="ui form error"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field
+          className="ui field"
+          name="title"
+          component={this.renderInput}
+          label="Enter title"
+        />
         <Field
           name="description"
           component={this.renderInput}
@@ -46,7 +70,20 @@ class StreamCreate extends React.Component {
   }
 }
 
+const validate = formValues => {
+  console.log("StreamCreate.validate(formValues) ", formValues);
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "You must enter a title.";
+  }
+  if (!formValues.description) {
+    errors.description = "You must enter a description.";
+  }
+  return errors;
+};
+
 // NB: Causes prop is Object not Function warning.
 export default reduxForm({
-  form: "streamCreate"
+  form: "streamCreate",
+  validate
 })(StreamCreate);
