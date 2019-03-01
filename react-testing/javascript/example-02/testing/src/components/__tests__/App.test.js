0d5e1477e272018373cd24a972d480abf273d2b5
Tim Langford <tim.langford@gmail.com>
2019-03-01 08:17:20 +0000

    Add forEach testing.

--- react-testing/javascript/example-02/testing/src/components/__tests__/App.test.js
+++ react-testing/javascript/example-02/testing/src/components/__tests__/App.test.js
@@ -5,16 +5,21 @@ import App from "components/App";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
-it("shows a comment box", () => {
+
+let wrapped;
+// Executed before each test. Include common test logic.
+beforeEach(() => {
     // Create a shallow JSDOM to prevent tests accessing child component information.
-    const wrapped = shallow(<App/>);
+     wrapped = shallow(<App/>);
+});
+
+it("shows a comment box", () => {
+
     // Just check for the recorded existence of a single child component.
     expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it("shows a comment list", () => {
-    // Create a shallow JSDOM to prevent tests accessing child component information.
-    const wrapped = shallow(<App/>);
     // Just check for the recorded existence of a single child component.
     expect(wrapped.find(CommentList).length).toEqual(1);
 });
