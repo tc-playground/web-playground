16279dda3adbaf8ebc9ca49b767351851d8499f9
Tim Langford <tim.langford@gmail.com>
2019-03-03 15:26:16 +0000

    Added CommentBox deep DOM tests.

---
+++ react-testing/javascript/example-02/testing/src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { mount } from "enzyme";
+import CommentBox  from "components/CommentBox";
+
+it ("has a textarea and a button", () => {
+    const wrapped = mount(<CommentBox/>);
+
+    // console.log("wrapped textarea: ", wrapped.find('textarea'));
+    // console.log("wrapped button: ", wrapped.find('button'));
+    
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
+    
+});
