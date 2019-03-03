71b67cf2de61935814887c9c05c79c002c6a8f19
Tim Langford <tim.langford@gmail.com>
2019-03-03 15:37:11 +0000

    Added test clean-up.

--- react-testing/javascript/example-02/testing/src/components/__tests__/CommentBox.test.js
+++ react-testing/javascript/example-02/testing/src/components/__tests__/CommentBox.test.js
@@ -2,13 +2,19 @@ import React from "react";
 import { mount } from "enzyme";
 import CommentBox  from "components/CommentBox";
 
-it ("has a textarea and a button", () => {
-    const wrapped = mount(<CommentBox/>);
+let wrapped;
 
+beforeEach( () => {
+    wrapped = mount(<CommentBox/>);
+});
+
+afterEach( () => {
+    wrapped.unmount();
+});
+
+it ("has a textarea and a button", () => {
     // console.log("wrapped textarea: ", wrapped.find('textarea'));
     // console.log("wrapped button: ", wrapped.find('button'));
-    
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
-    
 });
