78d155d0d2c505c4e52da35b9f8b0f1fdb0ef7b4
Tim Langford <tim.langford@gmail.com>
2019-03-05 19:52:25 +0000

    Testing adding comments in CommentBox.

--- react-testing/javascript/example-02/testing/src/reducers/comments.js
+++ react-testing/javascript/example-02/testing/src/reducers/comments.js
@@ -1,6 +1,6 @@
 import { SAVE_COMMENT } from "actions/types";
 
-export default (state = [], action) => {
+export default function(state = [], action) {
   switch (action.type) {
     case SAVE_COMMENT:
       return [...state, action.payload];
