94cad5c8c7ed5274cb2c01ce077bc81c329f515a
Tim Langford <tim.langford@gmail.com>
2019-03-18 07:38:36 +0000

    Show admin buttons.

--- react-streams-app/client/src/components/streams/StreamList.js
+++ react-streams-app/client/src/components/streams/StreamList.js
@@ -7,15 +7,27 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderAdmin = stream => {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  };
+
   renderStreams(props) {
     return this.props.streams.map(stream => {
       return (
-        <div className='item' key={stream.id}>
-            <i className='large middle aligned icon camera' />
-            <div className='content'>
-                {stream.title}
-                <div className='description'>{stream.description}</div>
-            </div>
+        <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
         </div>
       );
     });
@@ -23,17 +35,18 @@ class StreamList extends React.Component {
 
   render() {
     return (
-        <div>
-            <h2>Streams</h2>
-            <div className='ui celled list'>{this.renderStreams()}</div>
-        </div>
-    )
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderStreams()}</div>
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
-    streams: Object.values(state.streams)
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
   };
 };
 
