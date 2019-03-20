b24d033713774edd63b29e0d67c7ad7e35354d91
Tim Langford <tim.langford@gmail.com>
2019-03-20 00:44:38 +0000

    Stub edit stream functionality.

--- react-streams-app/client/src/components/streams/StreamEdit.js
+++ react-streams-app/client/src/components/streams/StreamEdit.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-const StreamEdit = () => {
-    return <div>StreamEdit</div>;
+import { connect } from 'react-redux';
+
+import { fetchStream } from '../../actions';
+
+class StreamEdit extends React.Component {
+
+    componentDidMount() {
+        fetchStream(this.props.match.params.id);
+    }
+
+    render() {
+        console.log("StreamEdit - props: ", this.props);
+        
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+        return <div>{this.props.stream.title}</div>;
+    }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps)(StreamEdit);
