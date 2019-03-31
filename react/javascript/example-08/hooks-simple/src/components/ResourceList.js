5da042ef53d1e1e2628b5c396ec01961b44eb564
Tim Langford <tim.langford@gmail.com>
2019-03-31 18:14:35 +0100

    Refactored ResourceList into hook based functional component.

--- react/javascript/example-08/hooks-simple/src/components/ResourceList.js
+++ react/javascript/example-08/hooks-simple/src/components/ResourceList.js
@@ -1,33 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-class ResourceList extends React.Component {
-  state = { resources: [] };
+const ResourceList = ({ resource }) => {
+  // class ResourceList extends React.Component {
+  //   state = { resources: [] };
+  //
+  //   async componentDidMount() {
+  //     const response = await axios.get(
+  //       `https://jsonplaceholder.typicode.com/${this.props.resource}`
+  //     );
+  //     this.setState({ resources: response.data });
+  //   }
+  //
+  //   async componentDidUpdate(previousProps) {
+  //     // We need to check that the props have changed as EVERYTIME setState() is called
+  //     // 'componentDidUpdate' will be executed.
+  //     //
+  //     if (previousProps.resource !== this.props.resource) {
+  //       const response = await axios.get(
+  //         `https://jsonplaceholder.typicode.com/${this.props.resource}`
+  //       );
+  //       this.setState({ resources: response.data });
+  //     }
+  //   }
+  //
+  //   render() {
+  //     return (
+  //       <div className="ui container">ResourceList: {this.props.resource}</div>
+  //     );
+  //   }
 
-  async componentDidMount() {
+  const [resources, setResource] = useState([]);
+
+  const fetchResource = async () => {
     const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      `https://jsonplaceholder.typicode.com/${resource}`
     );
-    this.setState({ resources: response.data });
-  }
+    setResource(response.data);
+  };
 
-  async componentDidUpdate(previousProps) {
-    // We need to check that the props have changed as EVERYTIME setState() is called
-    // 'componentDidUpdate' will be executed.
-    //
-    if (previousProps.resource !== this.props.resource) {
-      const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/${this.props.resource}`
-      );
-      this.setState({ resources: response.data });
-    }
-  }
+  useEffect(() => {
+    fetchResource(resource);
+  }, [resource]);
 
-  render() {
-    return (
-      <div className="ui container">ResourceList: {this.props.resource}</div>
-    );
-  }
-}
+  return (
+    <ul>
+        {resources.map((r) => 
+            <li key={r.id}>{r.title}</li>)}
+    </ul>);
+};
 
 export default ResourceList;
