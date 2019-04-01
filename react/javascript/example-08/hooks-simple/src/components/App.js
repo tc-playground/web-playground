4fe70553641216e4b99a3c0b0888c851131d9ba2
Tim Langford <tim.langford@gmail.com>
2019-04-01 23:58:40 +0100

    Added UserList and useResources module.

--- react/javascript/example-08/hooks-simple/src/components/App.js
+++ react/javascript/example-08/hooks-simple/src/components/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 
-import ResourceList from './ResourceList';
+import ResourceList from "./ResourceList";
+import UserList from "./UserList";
 
 const App = () => {
   const [resource, setResource] = useState("posts");
   return (
     <div>
       <div>
-        <button onClick={() => setResource('posts')}>
-          Post
-        </button>
-        <button onClick={() => setResource('todos')}>
-          Todos
-        </button>
+        <h3>Users</h3>
+        <UserList />
+      </div>
+      <h3>Resources</h3>
+      <div>
+        <button onClick={() => setResource("posts")}>Post</button>
+        <button onClick={() => setResource("todos")}>Todos</button>
       </div>
       <ResourceList resource={resource} />
     </div>
--- react/javascript/example-08/hooks-simple/src/components/ResourceList.js
+++ react/javascript/example-08/hooks-simple/src/components/ResourceList.js
@@ -1,53 +1,48 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from 'react';
+
+import useResources from './useResources';
+
+// class ResourceList extends React.Component {
+//   state = { resources: [] };
+//
+//   async componentDidMount() {
+//     const response = await axios.get(
+//       `https://jsonplaceholder.typicode.com/${this.props.resource}`
+//     );
+//     this.setState({ resources: response.data });
+//   }
+//
+//   async componentDidUpdate(previousProps) {
+//     // We need to check that the props have changed as EVERYTIME setState() is called
+//     // 'componentDidUpdate' will be executed.
+//     //
+//     if (previousProps.resource !== this.props.resource) {
+//       const response = await axios.get(
+//         `https://jsonplaceholder.typicode.com/${this.props.resource}`
+//       );
+//       this.setState({ resources: response.data });
+//     }
+//   }
+//
+//   render() {
+//     return (
+//       <div className="ui container">ResourceList: {this.props.resource}</div>
+//     );
+//   }
+
 
 const ResourceList = ({ resource }) => {
-  // class ResourceList extends React.Component {
-  //   state = { resources: [] };
-  //
-  //   async componentDidMount() {
-  //     const response = await axios.get(
-  //       `https://jsonplaceholder.typicode.com/${this.props.resource}`
-  //     );
-  //     this.setState({ resources: response.data });
-  //   }
-  //
-  //   async componentDidUpdate(previousProps) {
-  //     // We need to check that the props have changed as EVERYTIME setState() is called
-  //     // 'componentDidUpdate' will be executed.
-  //     //
-  //     if (previousProps.resource !== this.props.resource) {
-  //       const response = await axios.get(
-  //         `https://jsonplaceholder.typicode.com/${this.props.resource}`
-  //       );
-  //       this.setState({ resources: response.data });
-  //     }
-  //   }
-  //
-  //   render() {
-  //     return (
-  //       <div className="ui container">ResourceList: {this.props.resource}</div>
-  //     );
-  //   }
 
-  const [resources, setResource] = useState([]);
-
-  const fetchResource = async () => {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    );
-    setResource(response.data);
-  };
-
-  useEffect(() => {
-    fetchResource(resource);
-  }, [resource]);
+  const resources = useResources(resource);
 
   return (
     <ul>
-        {resources.map((r) => 
-            <li key={r.id}>{r.title}</li>)}
-    </ul>);
+      {resources.map(r => (
+        <li key={r.id}>{r.title}</li>
+      ))}
+    </ul>
+  );
 };
+
 
 export default ResourceList;
