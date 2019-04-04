cb34f5f5fa6bdb0239e69e25e3104aba298bfd0a
Tim Langford <tim.langford@gmail.com>
2019-04-04 11:35:31 +0100

    Converted app to functional component with hooks.

--- react/javascript/example-09/seasons-with-hooks/src/index.js
+++ react/javascript/example-09/seasons-with-hooks/src/index.js
@@ -1,33 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-class App extends React.Component {
-  state = { lat: null, errorMessage: '' };
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [errorMsg, setErrorMsg] = useState("");
 
-  componentDidMount() {
+  // Replaces 'componentDidMount'. As we do not need it to be called multiple times
+  // (e.g. no 'componentDidUpdate') we pass in '[]' as the second argument.
+  useEffect(() => {
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
+      position => setLat(position.coords.latitude),
+      err => setErrorMsg(err.message)
     );
-  }
+  }, []);
 
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-
-    return <Spinner message="Please accept location request" />;
-  }
-
-  render() {
-    return <div className="border red">{this.renderContent()}</div>;
-  }
+let content;
+if (errorMsg) {
+  content = <div>Error: {errorMsg}</div>;
+} else if (lat) {
+  content =  <SeasonDisplay lat={lat} />;
+} else {
+  content = <Spinner message="Please accept location request" />;
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+return <div className="border red">{content}</div>;
+
+};
+
+// class App extends React.Component {
+//   state = { lat: null, errorMessage: "" };
+//
+//   componentDidMount() {
+//     window.navigator.geolocation.getCurrentPosition(
+//       position => this.setState({ lat: position.coords.latitude }),
+//       err => this.setState({ errorMessage: err.message })
+//     );
+//   }
+//
+//   renderContent() {
+//     if (this.state.errorMessage && !this.state.lat) {
+//       return <div>Error: {this.state.errorMessage}</div>;
+//     }
+//
+//     if (!this.state.errorMessage && this.state.lat) {
+//       return <SeasonDisplay lat={this.state.lat} />;
+//     }
+//
+//     return <Spinner message="Please accept location request" />;
+//   }
+//
+//   render() {
+//     return <div className="border red">{this.renderContent()}</div>;
+//   }
+// }
+
+ReactDOM.render(<App />, document.querySelector("#root"));
