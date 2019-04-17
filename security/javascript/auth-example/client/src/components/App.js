ccc2511217cc9a2eb3528ee0fb7d22fdc5f2160e
Tim Langford <tim.langford@gmail.com>
2019-04-17 22:17:42 +0100

    Created basic unstyled header.

--- security/javascript/auth-example/client/src/components/App.js
+++ security/javascript/auth-example/client/src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from './Header';
 
-class App extends React.Component {
 
-    render() {
-        return <h1>Auth App</h1>
-    }
+export default ({ children }) => {
 
+    return (
+        <div>
+            <Header/>
+            { children }
+        </div>
+    );
 }
-
-export default App;
